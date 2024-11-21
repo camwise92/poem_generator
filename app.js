e5ea8c2f81f4336ac7ffa28d95fb4ec48c703891
camwise92 <cameroncarlisle1992@gmail.com>
2024-11-21 17:37:54 +0000

    Added public HTML and get-poem endpoint

--- app.js
+++ app.js
@@ -5,19 +5,23 @@ import chatCompletion from './services/openai.js';
 
 const app = express();
 
-app.get(
-    '/', 
-
-    (req, res) => {
-        res.send('Hello from my lovely new server!');
-    }
-);
+app.use(express.static('public'));
 
 app.get(
     '/test',
     async (req, res) => {
-        const response = await chatCompletion("Provide an amusing sentence.");
+        const response = await chatCompletion("Provide life altering advice in one sentence.");
         res.send(response);
+    }
+);
+
+app.get(
+    '/get-poem',
+    async (req, res) => {
+        const response = await chatCompletion(
+            "Write me a beautiful poem, maximum of 4 lines"
+        );
+        res.send({ poem: response });
     }
 );
 
