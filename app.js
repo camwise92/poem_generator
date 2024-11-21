2d54e54d0350cd22456ee6d96ea01fca20cb1995
camwise92 <cameroncarlisle1992@gmail.com>
2024-11-21 16:57:01 +0000

    Add OpenAI connection & test endpoint

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Setting up a basic server
 
 import express from 'express';
+import chatCompletion from './services/openai.js';
 
 const app = express();
 
@@ -8,7 +9,15 @@ app.get(
     '/', 
 
     (req, res) => {
-        res.send('Hello from my lovely new!');
+        res.send('Hello from my lovely new server!');
+    }
+);
+
+app.get(
+    '/test',
+    async (req, res) => {
+        const response = await chatCompletion("Provide an amusing sentence.");
+        res.send(response);
     }
 );
 
